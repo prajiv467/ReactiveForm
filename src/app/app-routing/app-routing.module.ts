6be7abdb9332700c315ea21e686a8584aefd49c8
prajiv467 <96563957+prajiv467@users.noreply.github.com>
2021-12-30 21:14:23 +0530

    Second commit

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,12 +3,16 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from '../dashboard/dashboard.component'
 import { AboutComponent } from '../about/about.component';
+import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
+import { SignupComponent } from '../signup/signup.component';
 
 const routes: Routes = [
-  {
-    path:'about',
-    component:AboutComponent,
-  },
+  { path:'about',component:AboutComponent},
+  { path:'login',component:LoginComponent},
+  { path:'register',component:RegisterComponent},
+  { path:'Dashboard',component:DashboardComponent},
+  { path:'signup',component:SignupComponent},
   ];
 
   @NgModule({
